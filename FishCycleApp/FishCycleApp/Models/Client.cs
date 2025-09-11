28417432dc8e2f0d2956c3c1ccbefd011ad24df0
sharonnakishaa <sharonnakishahezharuputri@mail.ugm.ac.id>
2025-09-11 16:12:25 +0700

    Client.cs: menambahkan kode

--- FishCycleApp/FishCycleApp/Models/Client.cs
+++ FishCycleApp/FishCycleApp/Models/Client.cs
@@ -8,5 +8,12 @@ namespace FishCycleApp.Models
 {
     internal class Client
     {
+        public Guid ClientId { get; set; }
+        public string Name { get; set; }
+        public string Contact { get; set; }
+        public string Address { get; set; }
+        public string Category { get; set; }
+
+        public List<Transaction> Transactions { get; set; } = new List<Transaction>();
     }
 }
