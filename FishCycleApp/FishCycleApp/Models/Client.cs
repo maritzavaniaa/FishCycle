4c42f42d429194ff551d91d04d93aa26afbc3e8f
sharonnakishaa <sharonnakishahezharuputri@mail.ugm.ac.id>
2025-09-11 16:12:58 +0700

    Client.cs: menambahkan kode

--- FishCycleApp/FishCycleApp/Models/Client.cs
+++ FishCycleApp/FishCycleApp/Models/Client.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace FishCycleApp.Models
 {
-    internal class Client
+    public class Client
     {
         public Guid ClientId { get; set; }
         public string Name { get; set; }
