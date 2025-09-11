0923733920d7613884541d7bb2f261c6c5673cee
sharonnakishaa <sharonnakishahezharuputri@mail.ugm.ac.id>
2025-09-11 16:19:07 +0700

    Report.cs: menambahkan kode

--- FishCycleApp/FishCycleApp/Models/Report.cs
+++ FishCycleApp/FishCycleApp/Models/Report.cs
@@ -6,7 +6,20 @@ using System.Threading.Tasks;
 
 namespace FishCycleApp.Models
 {
-    internal class Report
+    public class Report
     {
+        public Guid ReportId { get; set; }
+        public DateTime Date { get; set; }
+        public string SalesData { get; set; }
+
+        // Relasi ke Transaction
+        public List<Transaction> Transactions { get; set; } = new List<Transaction>();
+
+        public string GenerateSalesSummary()
+        {
+            decimal totalSales = Transactions.Sum(t => t.Amount);
+            SalesData = $"Sales Report ({Date.ToShortDateString()}): Total Sales = {totalSales:C}";
+            return SalesData;
+        }
     }
 }
