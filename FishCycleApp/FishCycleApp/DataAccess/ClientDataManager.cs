ce318f85e703f02c6f71201ce6606dfc04b368af
sharonnakishaa <sharonnakishahezharuputri@mail.ugm.ac.id>
2025-11-02 23:07:15 +0700

    feat: menambahkan CRUD dan DataAccess yang menerapkan Encapsulation (DatabaseConnection) dan Inheritance (ClientDataManager)

---
+++ FishCycleApp/FishCycleApp/DataAccess/ClientDataManager.cs
@@ -0,0 +1,125 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows;
+using FishCycleApp.Models;
+
+namespace FishCycleApp.DataAccess
+{
+    public class ClientDataManager : DatabaseConnection
+    {
+        public DataTable LoadClientData()
+        {
+            DataTable dt = new DataTable();
+            string sql = "select * from st_select_client()";
+
+            try
+            {
+                OpenConnection();
+                using (NpgsqlCommand cmd = new NpgsqlCommand(sql, conn))
+                {
+                    using (NpgsqlDataReader rd = cmd.ExecuteReader())
+                    {
+                        dt.Load(rd);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error loading client data: " + ex.Message, "FATAL ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+            return dt;
+        }
+
+        public int InsertClient(Client clientData)
+        {
+            string sql = "select * from st_insert_client(:_id, :_name, :_contact, :_address, :_category)";
+            int result = 0;
+
+            try
+            {
+                OpenConnection();
+                using (NpgsqlCommand cmd = new NpgsqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("_id", clientData.ClientID);
+                    cmd.Parameters.AddWithValue("_name", clientData.ClientName);
+                    cmd.Parameters.AddWithValue("_contact", clientData.ClientContact);
+                    cmd.Parameters.AddWithValue("_address", clientData.ClientAddress);
+                    cmd.Parameters.AddWithValue("_category", clientData.ClientCategory);
+                    result = (int)cmd.ExecuteScalar();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error inserting client data: " + ex.Message, "FATAL ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+            return result;
+        }
+
+        public int UpdateClient(Client clientData)
+        {
+            string sql = "select * from st_update_client(:_id, :_name, :_contact, :_address, :_category)";
+            int result = 0;
+
+            try
+            {
+                OpenConnection();
+                using (NpgsqlCommand cmd = new NpgsqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("_id", clientData.ClientID);
+                    cmd.Parameters.AddWithValue("_name", clientData.ClientName);
+                    cmd.Parameters.AddWithValue("_contact", clientData.ClientContact);
+                    cmd.Parameters.AddWithValue("_address", clientData.ClientAddress);
+                    cmd.Parameters.AddWithValue("_category", clientData.ClientCategory);
+                    result = (int)cmd.ExecuteScalar();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error updating client data: " + ex.Message, "FATAL ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+            return result;
+        }
+
+        public int DeleteClient(string clientID)
+        {
+            string sql = "select * from st_delete_client(:_id)";
+            int result = 0;
+
+            try
+            {
+                OpenConnection();
+                using (NpgsqlCommand cmd = new NpgsqlCommand(sql, conn))
+                {
+                    cmd.Parameters.AddWithValue("_id", clientID);
+                    result = (int)cmd.ExecuteScalar();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error deleting client data: " + ex.Message, "FATAL ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+            finally
+            {
+                CloseConnection();
+            }
+            return result;
+        }
+    }
+}
---
+++ FishCycleApp/FishCycleApp/DataAccess/DatabaseConnection.cs
@@ -0,0 +1,45 @@
+﻿using Npgsql;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Windows;
+
+namespace FishCycleApp.DataAccess
+{
+    public class DatabaseConnection
+    {
+        protected NpgsqlConnection conn;
+        private string connstring = "Host=localhost;Port=5432;Username=app_user;Password=app123;Database=FishCycleDB";
+
+        public DatabaseConnection()
+        {
+            conn = new NpgsqlConnection(connstring);
+        }
+
+        public void OpenConnection()
+        {
+            try
+            {
+                if (conn.State == ConnectionState.Closed)
+                {
+                    conn.Open();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error opening database connection: " + ex.Message, "FATAL ERROR", MessageBoxButton.OK, MessageBoxImage.Error);
+            }
+        }
+
+        public void CloseConnection()
+        {
+            if (conn.State == ConnectionState.Open)
+            {
+                conn.Close();
+            }
+        }
+    }
+}
