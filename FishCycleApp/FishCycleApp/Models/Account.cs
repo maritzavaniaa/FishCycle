f876af32e6bdeab519ee18f58491817f4aaf2357
sharonnakishaa <sharonnakishahezharuputri@mail.ugm.ac.id>
2025-11-03 04:29:25 +0700

    refactor: menyesuaikan properti Account model

--- FishCycleApp/FishCycleApp/Models/Account.cs
+++ FishCycleApp/FishCycleApp/Models/Account.cs
@@ -8,22 +8,9 @@ namespace FishCycleApp.Models
 {
     public class Account
     {
-        public Guid UserID { get; set; }
+        public string UserID { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
         public string Password { get; set; }
-
-        public virtual void Login(string email, string password)
-        {
-            if (Email == email && Password == password)
-                Console.WriteLine("Login success!");
-            else
-                Console.WriteLine("Login failed!");
-        }
-
-        public virtual void Logout()
-        {
-            Console.WriteLine("User logged out.");
-        }
     }
 }
