3758e9f63bf525fb92b50db9d9b0122cd54e3c5a
sharonnakishaa <sharonnakishahezharuputri@mail.ugm.ac.id>
2025-11-02 22:17:31 +0700

    refactor: menyesuaikan model Client dengan skema PostgreSQL

--- FishCycleApp/FishCycleApp/Models/Client.cs
+++ FishCycleApp/FishCycleApp/Models/Client.cs
@@ -8,11 +8,11 @@ namespace FishCycleApp.Models
 {
     public class Client
     {
-        public Guid ClientId { get; set; }
-        public string Name { get; set; }
-        public string Contact { get; set; }
-        public string Address { get; set; }
-        public string Category { get; set; }
+        public string ClientID { get; set; }
+        public string ClientName { get; set; }
+        public string ClientContact { get; set; }
+        public string ClientAddress { get; set; }
+        public string ClientCategory { get; set; }
 
         public List<Transaction> Transactions { get; set; } = new List<Transaction>();
     }
