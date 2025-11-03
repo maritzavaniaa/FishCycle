b6a4373f47b1c1f77679c0e37466d683967b9745
sharonnakishaa <sharonnakishahezharuputri@mail.ugm.ac.id>
2025-11-03 08:11:35 +0700

    feat: menambahkan StockOut model

---
+++ FishCycleApp/FishCycleApp/Models/StockOutModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishCycleApp.Models
+{
+    public class StockOutModel
+    {
+        public string StockOutID { get; set; }
+        public string ProductID { get; set; }
+        public string TransactionID { get; set; }
+        public DateTime DateOut { get; set; }
+        public int QuantityOut { get; set; }
+    }
+}
