faa2aced16928a3c5648ab64a693442f2de7046d
sharonnakishaa <sharonnakishahezharuputri@mail.ugm.ac.id>
2025-11-03 08:06:16 +0700

    feat: menambahkan StockIn model

---
+++ FishCycleApp/FishCycleApp/Models/StockInModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishCycleApp.Models
+{
+    internal class StockInModel
+    {
+        public string StockInID { get; set; }
+        public string ProductID { get; set; }
+        public string SupplierID { get; set; }
+
+        public DateTime DateIn { get; set; }
+        public int QuantityIn { get; set; }
+    }
+}
