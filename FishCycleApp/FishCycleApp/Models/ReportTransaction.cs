abaf92c03c8f99ad23b4639f682d9e6a32b9703d
sharonnakishaa <sharonnakishahezharuputri@mail.ugm.ac.id>
2025-11-03 08:21:22 +0700

    feat: menambahkan ReportTransaction model

---
+++ FishCycleApp/FishCycleApp/Models/ReportTransaction.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FishCycleApp.Models
+{
+    public class ReportTransaction
+    {
+        public string ReportTransactionID { get; set; }
+        public string ReportID { get; set; }
+        public string TransactionID { get; set; }
+    }
+}
